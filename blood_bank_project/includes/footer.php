408aa0b3d8379523a9d197f7c73c824f04451338
M-Israk-Ahmed <50460692+M-Israk-Ahmed@users.noreply.github.com>
2020-04-11 19:22:50 +0600

    Update footer.php

--- blood_bank_project/includes/footer.php
+++ blood_bank_project/includes/footer.php
@@ -2,7 +2,7 @@
   
   <footer style="height:50px ;background-color:#292b2c" >
         <div >
-           <center> <p style="color:white;padding:10px"> Copyright&copy Team HamgMen</p></center>
+           <center> <p style="color:white;padding:10px"> &copy2020 Copyright. All Rightes Reserved by Team HamgMen</p></center>
         </div>
     </footer>
 	<!--" color:white;
